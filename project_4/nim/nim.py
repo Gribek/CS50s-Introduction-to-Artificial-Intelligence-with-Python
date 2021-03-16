340d0789ee4c72da016fe9b3f15d375c348a88f9
Gribek <piotrmaslewski@gmail.com>
2021-03-16 16:09:32 +0100

    Fix bug in best_future_reward method. Return zero when no actions are possible in the state.

--- project_4/nim/nim.py
+++ project_4/nim/nim.py
@@ -133,7 +133,8 @@ class NimAI():
         `state`, return 0.
         """
         actions = Nim.available_actions(state)
-        return max(self.q.get((state, a), default=0) for a in actions)
+        return max((self.q.get((state, a), default=0) for a in actions),
+                   default=0)
 
     def choose_action(self, state, epsilon=True):
         """
