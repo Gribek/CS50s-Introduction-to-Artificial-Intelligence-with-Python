9e6622a8bbe6ee6958cb737e8fc0af9a4c855959
Gribek <piotrmaslewski@gmail.com>
2021-03-16 17:22:49 +0100

    Refactor get_q_value method.

--- project_4/nim/nim.py
+++ project_4/nim/nim.py
@@ -101,8 +101,7 @@ class NimAI():
         Return the Q-value for the state `state` and the action `action`.
         If no Q-value exists yet in `self.q`, return 0.
         """
-        q_value = self.q.get((tuple(state), action))
-        return q_value if q_value is not None else 0
+        return self.q.get((tuple(state), action), 0)
 
     def update_q_value(self, state, action, old_q, reward, future_rewards):
         """
