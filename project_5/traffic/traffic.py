170bc4afe755bd327efd60a686927443bac50af6
Gribek <piotrmaslewski@gmail.com>
2021-03-18 19:41:08 +0100

    Adjust model

--- project_5/traffic/traffic.py
+++ project_5/traffic/traffic.py
@@ -85,10 +85,12 @@ def get_model():
                                input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
 
         tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
         tf.keras.layers.Flatten(),
 
-        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dense(128, activation='relu'),
         tf.keras.layers.Dropout(0.3),
 
         tf.keras.layers.Dense(NUM_CATEGORIES, activation='softmax')
